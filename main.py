a1989b1b37ff45f7a36a7e8d9f0b1c7a86bfd3cc
Jamalbek <32876973+Zhamalbek@users.noreply.github.com>
2024-10-26 12:04:54 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 from typing import Optional
 from fastapi import FastAPI
-import instaloader
+from openai import OpenAI
 
-L = instaloader.Instaloader()
-username = 'likelook.by'
-profile = instaloader.Profile.from_username(L.context, username)
-sss='';
-for post in profile.get_posts():
-    print(post.caption)
-    sss=sss+post.caption
-    
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/ins/{item_id}")
 def ins_item(item_id: str, q: Optional[str] = None):
-    global sss
-    return {"message": sss}
+    return {"message": item_id}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    global sss
-    return {"item_id": item_id, "q": q,"sss":sss}
+def read_item(item_id: int, q):
+    base_url = "https://api.aimlapi.com/v1"
+    system_prompt = "You are a travel agent. Be descriptive and helpful."
+    user_prompt = "Tell me about San Francisco"
+    api2 = OpenAI(api_key=api_key, base_url=base_url)
+    completion = api2.chat.completions.create(
+        model="mistralai/Mistral-7B-Instruct-v0.2",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        temperature=0.7,
+        max_tokens=256,
+    )
+
+    response = completion.choices[0].message.content
+
+    print("User:", user_prompt)
+    print("AI:", response)
+    q=user_prompt+'--'+response
+    return {"item_id": item_id, "q": q}
+@app.get("/{item_id}")
+def vvv_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
